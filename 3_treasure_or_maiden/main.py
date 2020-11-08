928bdf49761a02fad7c38bc6060a678ed001ae9b
aa-ronn <55965197+aa-ronn@users.noreply.github.com>
2020-11-07 20:26:01 -0500

    Day 3
    Nested conditionals, and, or

---
+++ 3_treasure_or_maiden/main.py
@@ -0,0 +1,241 @@
+treasure = '''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+'''
+
+dragon = """
+                                                       ____________
+                                 (`-..________....---''  ____..._.-`
+                                  \\`._______.._,.---'''     ,'
+                                  ; )`.      __..-'`-.      /
+                                 / /     _.-' _,.;;._ `-._,'
+                                / /   ,-' _.-'  //   ``--._``._
+                              ,','_.-' ,-' _.- (( =-    -. `-._`-._____
+                            ,;.''__..-'   _..--.\\.--'````--.._``-.`-._`.
+             _          |\,' .-''        ```-'`---'`-...__,._  ``-.`-.`-.`.
+  _     _.-,'(__)\__)\-'' `     ___  .          `     \      `--._
+,',)---' /|)          `     `      ``-.   `     /     /     `     `-.
+\_____--.  '`  `               __..-.  \     . (   < _...-----..._   `.
+ \_,--..__. \\ .-`.\----'';``,..-.__ \  \      ,`_. `.,-'`--'`---''`.  )
+           `.\`.\  `_.-..' ,'   _,-..'  /..,-''(, ,' ; ( _______`___..'__
+                   ((,(,__(    ((,(,__,'  ``'-- `'`.(\  `.,..______   SSt
+                                                      ``--------..._``--.__
+"""
+pegasus = """
+                                    ,
+                                   |`\
+                                  /'_/_
+                                ,'_/\_/\_                       ,
+                              ,'_/\'_\_,/_                    ,'|
+                            ,'_/\_'_ \_ \_/                _,-'_/
+                          ,'_/'\_'_ \_ \'_,\           _,-'_,-/ \,
+                        ,' /_\ _'_ \_ \'_,/       __,-'<_,' _,\_,/
+                       ( (' )\/(_ \_ \'_,\   __--' _,-_/_,-',_/ _\
+                        \_`\> 6` 7  \'_,/ ,-' _,-,'\,_'_ \,_/'_,\
+                          \/-  _/ 7 '/ _,' _/'\_  \,_'_ \_ \'_,/
+                           \_'/>   7'_/' _/' \_ '\,_'_ \_ \'_,\
+                             >/  _ ,V  ,<  \__ '\,_'_ \_ \'_,/
+                           /'_  ( )_)\/-,',__ '\,_'_,\_,\'_\
+                          ( ) \_ \|_  `\_    \_,/'\,_'_,/'
+                           \\_  \_\_)    `\_
+                            \_)   >        `\_
+                                 /  `,      |`\_
+                                /    \     / \ `\
+                               /   __/|   /  /  `\
+                              (`  (   (` (_  \   /
+                              /  ,/    |  /  /   \
+                             / ,/      | /   \   `\_
+                           _/_/        |/    /__/,_/     mic
+                          /_(         /_(
+"""
+
+island = """
+                                                                     .
+                                     .
+          ,d88b,                                     .                   __..-
+          888888                                        .            .--SEAL:.
+          `?88P'         .                       __                ,'WWII::.
+                                               .MW:`-.            /WWII::..
+                                       .    _.MWII:'. `.     .  ,'WII::..
+                                        _.-MWII::'.     `-.   ,'WWI::.
+    .                          _..vvvv,'WWII::'            `.'WII::.
+                            ,-'WI:'''/WII:'.                 \WI:.
+--------------------.==.--,'WWI:'.  /WI:'.          :.        `.I:.
+            -.      (88),'WWI:'.  ,'I:.              ::.        \..
+     -    ~-~_~ _./d88P/\?I:'.   /WI:.                ::.        \ .
+          ~-~-,-~' ~~~ / )------'WI:.____ _.d88P_____::::.        \
+   _       ,-'  .     /
+"""
+boat = """
+                   ~;
+                  ,/|\,
+                ,/' |\ \,
+              ,/'   | |  \
+            ,/'     | |   |
+          ,/'       |/    |
+        ,/__________|-----' ,
+       ___.....-----''-----/
+ jgs   \                  /
+   ~~-~^~^~`~^~`~^^~^~-^~^~^~-~^~^
+     ~-^~^-`~^~-^~^`^~^-^~^`^~^-~^
+"""
+beast = """
+                   (    )
+                  ((((()))
+                  |o\ /o)|
+                  ( (  _')
+                   (._.  /\__
+                  ,\___,/ '  ')
+    '.,_,,       (  .- .   .    )
+     \   \\     ( '        )(    )
+      \   \\    \.  _.__ ____( .  |
+       \  /\\   .(   .'  /\  '.  )
+        \(  \\.-' ( /    \/    \)
+         '  ()) _'.-|/\/\/\/\/\|
+             '\\ .( |\/\/\/\/\/|
+               '((  \    /\    /
+               ((((  '.__\/__.')
+                ((,) /   ((()   )
+                 "..-,  (()("   /
+            pils  _//.   ((() ."
+          _____ //,/" ___ ((( ', ___
+                           ((  )
+                            / /
+                          _/,/'
+                        /,/,"
+"""
+
+maiden = """
+         |><|~|><|
+         /(((9)))\
+        //) -_- (\\
+       (((( ._. ))))
+        ))))---((((
+       ((((`---'))))
+      (___|xXxXx|___)
+        \ |     | /
+         / ^ ^ ^ \
+        /         \
+       (_._._._._._)
+          \  |  /
+          (  |  )
+           | | |
+   hjw     |-|-|
+          /`-^-'\
+         (__,^.__)
+"""
+mountains = """
+                _,_           +                   __
+                ','                  /\          `. `.
+          .                        .'  \    +      "  |
+                                  /     \         /  .'         .
+                       .'\      .'       \       `"`
+      +             .-'   `.   /          `.
+            .     .'        \.'             \
+               .-'           \               \   .-`"`-.      . +
+           .'.'               \               \.'       `-.
+          /                    `.           .-'\           `-._
+        .'                       \       .-'                   `-.
+                                                                  `-.
+   .-------------------'''''''''''''''              _.--      .'
+                                ___..         _.--''        .'jb
+                          --''''             '            .'
+"""
+
+mole = """
+                              _.-=-._                                    
+                           o~`  '  > `.                                   
+                           `.  ,       :                                   
+                            `"-.__/    `.                                   
+                               /       ::                                  
+                              / .:    .:|                                 
+                             :       .::!.                               
+                            /'| ::  :::'!!                               
+                          .:  :/' .::/  !!                               
+                          :::/   :::'   !!                               
+                          `:"::'''!!    !!                                 
+                            /          :!!.                                
+                           /     .-~-:  !!!                                
+                          /:   :'        !!.                               
+                         :::  :'          !!                               
+                         |::  |        :!!!!                               
+                         `::  :        !!!!'                               
+                          |:. `:    .  '!!!                                
+                          `::.  \   `::. !'                                
+                           _.`::.\     ::                                  
+                        .-~_____:~~    :'                                  
+                        ~~~  .-'__..-~'           JaL                         
+                             ~~~
+"""
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+import time
+
+
+print("Welcome! You must find the treasure.")
+print("...or you can save the maiden.\n\n") 
+
+print(mountains)
+
+print("You are standing in a large grass field. there are mountains in the distance. To your left the grasslands continue, to the right there is a cave.")
+choice = input("Do you want to go left or right?").lower()
+
+
+if choice == "r" or choice == "right":
+  print("You enter the cave after lighting a tourch. It is dark and cold. You hear a rumble deep in the cave...You decide to turn and run, but it is too late. A dragon a appears and eats you. Game over")
+  print(dragon)
+  print("I hope you like your steak well done...")
+else:
+  print(island)
+  print("You walked for miles and miles eventually coming upon an vast body of water. There is a small island in the distance. You see a boat to the left and a small village to the right")
+  choice = input("Do you want to go left or right?").lower()
+  if choice == "r" or choice == "right":
+    print("You make your way to the small village, but as you approach something feels off...the ground is shaking! You look down and see multiple pairs of small hands grabing you by the feet. You are pulled into the Earth and buried alive by sentient moles. Your final though was 'Atleast they were cute'")
+    print(mole)
+    print("I'd buy ya a drink, but I'm a miner...")
+  else:
+    print(boat)
+    print("You drag the near by boat into the water and set sail for the island...")
+    for x in range(0, 10):
+      b = "The trip is long" + "." * x
+      print(b, end="\r")
+      time.sleep(.200)
+    print("when you arrive on the island you imediately notice a tall tower atop the highest hill. This will be a long walk. To your left is a forest, you can begin your walk here. To your right appears to be a road, this may make for an easier journey.")
+    choice = input("Do you want to go left or right?").lower()
+    if choice == "r" or choice == "right":
+      print("You arrive on the road. There is a small bridge ahead so you can easily cross the river. You smell something in the air...A horrible vile rotten beast comes out from under the bridge! You attempt to speak with the creature, but the beast assumes a small human like yourself can not pay his, what you assume to be, outrageous troll toll. You are but to pieces")
+      print(beast)
+      print("Smell ya later...")
+    else:
+      print(pegasus)
+      print("This story is getting too long...wrap it up.")
+      print("You head into the forest and encounter a herd a very kind pegasi. They are so kind infact, they agree to fly you too the tall tower")
+      print("While flying over the beautiful forest you see a glimmer of light to your left. To your right is the tall tower.")
+      choice = input("Do you want to go left or right?").lower()
+      if choice == "l" or choice == "left":
+        print("As you get closer you can clearly see that the glimmer was caused by a massive pile of treasure. In your excitment your hands become sweaty, you lose your grip on the pegasus' mane...you fall to your death. The pegasus is startled by this, but to not appear like a bad pegaus it flies home and tells the others the human is safe. Typical.")
+        print(treasure)
+        print("Knees weak, palms are sweaty...")
+      else:
+        print(maiden)
+        print("You arrive at the tall tower. The pegasus takes you up to the top. A beautiful maiden awaits you. You begin to get off the pegasus, your foot slips, you begin to fall, your chin hits the rail of the balcony, you black out...\n\n")
+        time.sleep(2000)
+        print("Well that is unfortunate...")
